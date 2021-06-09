4a48bbf5fdb10d02b297af7d2334cae3f8d76442
fractaaal <b1329180@gmail.com>
2021-06-09 22:00:01 +0900

    Time: 196 ms (6.93%), Space: 44.6 MB (26.91%) - LeetHub

---
+++ valid-anagram/valid-anagram.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    return (sortStr(s) === sortStr(t));
+    
+};
+var sortStr = (str)=>{
+    return str.split('').sort().join('')        
+}
